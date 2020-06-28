620c5a8442f8245268b435610dc147116412b176
tianyu5177 <zhangtianyu5177@sina.com>
2020-06-28 11:05:57 +0800

    更新

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View,Button } from 'react-native'
 
 export default class Home extends Component {
+	static navigationOptions = {
+		title:'主页'
+	}
 	render() {
 		return (
-			<View style={{flex:1,backgroundColor:'skyblue'}}>
+			<View style={{flex:1,backgroundColor:'skyblue',alignItems:'center'}}>
 				<Text style={{fontSize:20}}> 我是Home组件 </Text>
+				<View style={{width:150,marginTop:100}}>
+					<Button title="点我去热门页面" onPress={()=>{this.props.navigation.push('Hot')}}/>
+				</View>
 			</View>
 		)
 	}
--- pages/Hot/Hot.js
+++ pages/Hot/Hot.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View,Button } from 'react-native'
 
 export default class Hot extends Component {
+	static navigationOptions = {
+		title:'热门',
+		headerLeft:()=>(
+			<View style={{marginLeft:10}}>
+				<Button
+					title="扫一扫"
+					color="red"
+				/>
+			</View>
+		),
+		headerRight: () => ( //导航右侧按钮
+			<View style={{marginRight:10}}>
+				<Button
+					onPress={() => alert('This is a button!')}
+					title="点我"
+					color="black"
+				/>
+			</View>
+    ),
+	}
 	render() {
 		return (
-			<View style={{flex:1,backgroundColor:'pink'}}>
+			<View style={{flex:1,backgroundColor:'pink',alignItems:'center'}}>
 					<Text style={{fontSize:20}}> 我是Hot组件 </Text>
+					<View style={{width:150,marginTop:100}}>
+					<Button 
+						title="点我回主页" 
+						color="green" 
+						onPress={()=>{this.props.navigation.goBack()}}
+					/>
+					<Button 
+						title="点我去热门" 
+						color="orange" 
+						onPress={()=>{this.props.navigation.navigate('Hot')}}
+					/>
+				</View>
 			</View>
 		)
 	}
